ea4dbb249ea51928321d4d12cf656cdaa6543068
CountZero0 <97768163+CountZero0@users.noreply.github.com>
2024-01-26 20:12:45 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# урл тестового стенда
+BASE_URL = "https://4c60fb5a-a2b0-4740-a6b6-228ec7ae6608.serverhub.praktikum-services.ru"
+# эндпоинт создания заказа
+CREATE_ORDER_PATH = "/api/v1/orders"
+# эндпоинт получения данных о заказе по его номеру
+GET_ORDER_PATH = "/api/v1/orders/track?t="
