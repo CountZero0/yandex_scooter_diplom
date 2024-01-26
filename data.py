367424c8374a82d9bd751dcdac38de9292a8eda6
CountZero0 <97768163+CountZero0@users.noreply.github.com>
2024-01-26 20:16:18 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Константин",
+    "lastName": "Давыдочкин",
+    "address": "Охотный ряд 2",
+    "metroStation": 204,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-01-28",
+    "comment": "Привет",
+    "color": [
+        "BLACK"
+    ]
+}
